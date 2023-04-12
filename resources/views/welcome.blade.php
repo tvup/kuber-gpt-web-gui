5b4f6f4a819da7e85f18483f11ce7ae50e7b340a
Torben Evald Hansen <contact@torbenit.dk>
2023-04-13 01:06:35 +0200

    Refactor welcome.blade.php to improve user experience and add gradient background animation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,43 +1,58 @@
 @extends('layouts.app')
 
 @section('content')
-    <div id="app" class="flex-center position-ref full-height">
-        @if (Route::has('login'))
-            <div class="top-right links">
-                @auth
-                    <a href="{{ url('/home') }}">Home</a>
-                @else
-                    <a href="{{ route('login') }}">Login</a>
-                    <!-- <a href="{{ route('register') }}">Register</a> -->
-                @endauth
-            </div>
-        @endif
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card border-0 shadow-lg">
+                    <div class="card-body text-center">
+                        <h1 class="display-4 mb-4 text-primary">{{ __('welcome.vpn_manager') }}</h1>
+                        <p class="lead">{{ __('welcome.prato_commune') }}</p>
+                        <p>{{ __('welcome.certificate_manager_and_related_users') }}</p>
+                    </div>
 
-        <div class="content">
-            <div class="title m-b-md">
-                {{ __('welcome.vpn_manager') }}
-            </div>
-            <div>
-                {{ __('welcome.prato_commune') }}
-            </div>
-            <div class="m-b-md">
-                {{ __('welcome.certificate_manager_and_related_users') }}
-            </div>
+                    @guest
+                        <div class="d-flex justify-content-center mb-3">
+                            <a class="btn btn-primary mr-2" href="{{ route('login') }}">Login</a>
+                        </div>
+                    @else
+                        <div class="d-flex justify-content-center mb-3">
+                            <a class="btn btn-success mr-2" href="{{ route('home') }}"> Dashboard </a>
+                            <a class="btn btn-primary" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
 
-            @guest
-                <a class="btn btn-primary" href="{{ route('login') }}">Login</a>
-            @else
-                <a class="btn btn-success" href="{{ route('home') }}"> Dashboard </a>
-                <a class="btn btn-primary" href="{{ route('logout') }}"
-                   onclick="event.preventDefault();
-                            document.getElementById('logout-form').submit();">
-                    {{ __('Logout') }}
-                </a>
-
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                    @csrf
-                </form>
-            @endguest
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </div>
+                    @endguest
+                    <div class="card-footer bg-transparent">
+                        <p class="text-muted">&copy; {{ date('Y') }} {{ __('welcome.prato_commune') }}</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
+    <style>
+        body {
+            background: linear-gradient(135deg, #7f7fd5, #86a8e7, #91eae4);
+            background-size: 600% 600%;
+            animation: gradient 15s ease infinite;
+        }
+
+        @keyframes gradient {
+            0% {
+                background-position: 0% 50%;
+            }
+            50% {
+                background-position: 100% 50%;
+            }
+            100% {
+                background-position: 0% 50%;
+            }
+        }
+    </style>
 @endsection

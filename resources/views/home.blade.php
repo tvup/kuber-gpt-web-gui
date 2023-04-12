ce95d51ec655d071198e2c5dcbc2938a9e06784f
Torben Evald Hansen <contact@torbenit.dk>
2023-04-13 00:55:02 +0200

    Refactor home and welcome views to improve readability and accessibility

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,72 +1,67 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{__('home.dashboard')}} - {{ $role }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card shadow-lg">
+                    <div class="card-header bg-primary text-white">{{__('home.dashboard')}} - {{ $role }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
 
-                    @if(($role == \App\Enums\UserRoleEnum::Admin) || ($role == \App\Enums\UserRoleEnum::Manager))
-                    <ul>
+                        @if(($role == \App\Enums\UserRoleEnum::Admin) || ($role == \App\Enums\UserRoleEnum::Manager))
+                            <ul class="list-group list-group-flush">
 
-                        <li><a href={{route('admin.admin_popolatedb')}}> {{ __('home.open_certificate_dashboard') }}</a></li>
-                        <li><a href={{route('admin.admin_showallusers')}}> {{ __('home.show_all_registered_users') }}</a></li>
+                                <li class="list-group-item"><a href={{route('admin.admin_popolatedb')}}> {{ __('home.open_certificate_dashboard') }}</a></li>
+                                <li class="list-group-item"><a href={{route('admin.admin_showallusers')}}> {{ __('home.show_all_registered_users') }}</a></li>
 
-                    </ul>
+                            </ul>
 
-                    @else
+                        @else
 
-                    <ul>
-                        <li><a href="{{ route('user_downloadmycert') }}" class="btn btn-success">
-                                {{ __('home.download_vpn_access') }}
-                                <i class="fas fa-download"></i>
-                            </a>
-                        </li>
-                        <hr>
-                        <li>
-                            <a href="https://swupdate.openvpn.org/community/releases/openvpn-install-2.4.6-I602.exe" class="btn btn-info">
-                                {{ __('home.download_openvpn_client') }}
-                                <i class="fas fa-download"></i>
-                            </a></li>
-                        <hr>
-                        <li>
-                            <a href="#" class="btn btn-primary">
-                                {{__('home.guide_in_progress')}}
-                                <i class="fas fa-spinner"></i>
-                            </a>
-                        </li>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">
+                                    <a href="{{ route('user_downloadmycert') }}" class="btn btn-success">
+                                        {{ __('home.download_vpn_access') }}
+                                        <i class="fas fa-download"></i>
+                                    </a>
+                                </li>
+                                <hr>
+                                <li class="list-group-item">
+                                    <a href="https://swupdate.openvpn.org/community/releases/openvpn-install-2.4.6-I602.exe" class="btn btn-info">
+                                        {{ __('home.download_openvpn_client') }}
+                                        <i class="fas fa-download"></i>
+                                    </a>
+                                </li>
+                                <hr>
+                                <li class="list-group-item">
+                                    <a href="#" class="btn btn-primary">
+                                        {{__('home.guide_in_progress')}}
+                                        <i class="fas fa-spinner"></i>
+                                    </a>
+                                </li>
 
-                        <hr>
-                        <li>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST">
+                                <hr>
+                                <li class="list-group-item">
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST">
                                         @csrf
-                                <button type="submit" class="btn btn-danger">
-                                    {{ __('auth.logout') }}
-                                    <i class="fas fa-sign-out-alt"></i>
-                                </button>
+                                        <button type="submit" class="btn btn-danger">
+                                            {{ __('auth.logout') }}
+                                            <i class="fas fa-sign-out-alt"></i>
+                                        </button>
+                                    </form>
+                                </li>
+                            </ul>
 
-                            </form>
-
-                        </li>
-
-
-
-                    @endif
-
-
-
-
+                        @endif
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection

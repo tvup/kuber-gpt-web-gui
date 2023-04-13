838b5a0e19aabbb69c79fae81891ee6a228bd8fd
Torben Evald Hansen <contact@torbenit.dk>
2023-04-13 22:45:48 +0200

    Add dynamic fields for applications and tags in server assets creation form

--- resources/lang/en/create.php
+++ resources/lang/en/create.php
@@ -9,20 +9,6 @@ return [
     'public_ip' => 'Public IP',
     'applications' => 'Applications',
     'tags' => 'Tags',
-
-    'vat_number' => 'VAT number',
-    'company' => 'Company',
-    'e_mail' => 'E-Mail',
-    'email_address_unique' => 'E-Mail Address - Unique per login',
-    'privileges' => 'Privileges',
-    'admin' => 'Admin',
-    'manager' => 'Manager - read only',
-    'user' => 'User',
-    'password' => 'Password',
-    'confirm_password' => 'Confirm Password',
-    'show_password' => 'Show Password',
-    'suggest_password' => 'Suggest a password',
-    'TS' => 'TS',
-    'FULL' => 'FULL',
+    'reset' => 'Return to list of servers',
 
 ];

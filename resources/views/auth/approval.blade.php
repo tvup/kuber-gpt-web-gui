08b319a9a9a7dc841ffb0000e161348476051e88
Torben Evald Hansen <contact@torbenit.dk>
2023-04-22 02:58:50 +0200

    Refactor approval blade template to include namespace in language keys

--- resources/views/auth/approval.blade.php
+++ resources/views/auth/approval.blade.php
@@ -5,12 +5,12 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">{{ __('waiting_for_approval') }}</div>
+                    <div class="card-header">{{ __('approval.waiting_for_approval') }}</div>
 
                     <div class="card-body">
-                        {{ __('account_review') }}
+                        {{ __('approval.account_review') }}
                         <br />
-                        {{ __('check_back_later') }}
+                        {{ __('approval.check_back_later') }}
                     </div>
                 </div>
             </div>

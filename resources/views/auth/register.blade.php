1382b7d12c2f2efb5f2bd15e73219fcb33b7c33a
Torben Evald Hansen <contact@torbenit.dk>
2023-04-22 02:52:30 +0200

    Fix typographical error in registration form's confirm password label

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -54,7 +54,7 @@
                             </div>
 
                             <div class="row mb-3">
-                                <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('regoster.confirm_password') }}</label>
+                                <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('register.confirm_password') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">

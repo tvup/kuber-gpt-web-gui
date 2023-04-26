158677c317275a77f91c10fa3eeccc1764c2b30e
Torben Evald Hansen <contact@torbenit.dk>
2023-04-27 01:37:18 +0200

    Add payment method ID to form submission in register2 template

--- resources/views/auth/register2.blade.php
+++ resources/views/auth/register2.blade.php
@@ -105,6 +105,8 @@
             alert('Something went wrong: ' + error);
         } else {
             document.getElementById('pmi').value = setupIntent.payment_method;
+            console.log('From response: ' + setupIntent.payment_method);
+            console.log('From element: ' + document.getElementById('pmi').value);
             document.getElementById("reg-form").submit();
 
             //window.location.replace("https://kuber-gpt.com/subscribe/" + {{ $user_id }}+ "/" + setupIntent.payment_method);

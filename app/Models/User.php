b77690530dce74f785656c7087de817ff62ee779
Torben Evald Hansen <contact@torbenit.dk>
2023-04-27 01:12:18 +0200

    Update RegisterController and User model to include allowed_a_is and a_is_running fields in user registration in RegisterController.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,7 +46,7 @@ class User extends Authenticatable
      * @var array<string>
      */
     protected $fillable = [
-        'user_name', 'email', 'password', 'vat_number', 'name', 'surname', 'role', 'password_clear', 'company', 'approved_at',
+        'user_name', 'email', 'password', 'vat_number', 'name', 'surname', 'role', 'password_clear', 'company', 'approved_at', 'allowed_a_is', 'a_is_running',
     ];
 
     /**

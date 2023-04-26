733249a3f0a4d05d908886a4a66278905abd28d9
Torben Evald Hansen <contact@torbenit.dk>
2023-04-27 01:42:54 +0200

    Remove unnecessary alert messages and submit form after setting payment method in register2.blade.php

--- resources/views/auth/register2.blade.php
+++ resources/views/auth/register2.blade.php
@@ -106,8 +106,6 @@
             alert('Something went wrong: ' + error);
         } else {
             document.getElementById('pmi').value = setupIntent.payment_method;
-            alert('From response: ' + setupIntent.payment_method);
-            alert('From element: ' + document.getElementById('pmi').value);
             document.getElementById("reg-form").submit();
 
             //window.location.replace("https://kuber-gpt.com/subscribe/" + {{ $user_id }}+ "/" + setupIntent.payment_method);

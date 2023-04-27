360570e4ca87916f48d0ab7e201c43812c32ecc6
Torben Evald Hansen <contact@torbenit.dk>
2023-04-27 18:18:15 +0200

    Add name field to User creation in RegisterController's create method

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -120,6 +120,7 @@ class RegisterController extends Controller
     protected function create(array $data): User
     {
         return User::create([
+            'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
             'role' => UserRoleEnum::User,

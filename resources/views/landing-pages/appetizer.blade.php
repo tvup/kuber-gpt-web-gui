278dd04272d5e1556ca91147332d9ec2b0061a06
Torben Evald Hansen <contact@torbenit.dk>
2023-04-28 01:32:22 +0200

    Add disclaimer message for beta software in appetizer landing page

--- resources/views/landing-pages/appetizer.blade.php
+++ resources/views/landing-pages/appetizer.blade.php
@@ -3,7 +3,20 @@
 @section('title', $title)
 
 @section('content')
+
     <div class="flex bg-white" style="height:600px;">
+        <div class="content-top"  style="position:absolute;
+ top:144px;
+ right:-62px;
+ left:205px;
+ color:rgb(0, 0, 0);
+ font-size:23px;
+ background-color:#f6f655;
+ width:586px;
+ height:88px !important;
+ background-position-y:0%;
+ overflow:visible;
+ min-height:120px;"><p>DISCLAIMER - This software is to be considered beta!<br/>A lot of these technologies are baby-new and will not work flawlessly or smoothly</p></div>
         <div class="flex items-center text-center lg:text-left px-8 md:px-12 lg:w-1/2">
             <div>
                 <h2 class="text-3xl font-semibold text-gray-800 md:text-4xl">AutoGPT <span class="text-indigo-600">get ready!</span>

716cf1ebe2a530107dfb99903d3609846ffb6ead
Torben Evald Hansen <contact@torbenit.dk>
2023-04-27 03:20:26 +0200

    Add functionality to launch a specific RunSet by ID in ConductorController's launch method

--- app/Http/Controllers/ConductorController.php
+++ app/Http/Controllers/ConductorController.php
@@ -12,7 +12,9 @@ class ConductorController extends Controller
     /**
      * @throws RedisException
      */
-    public function launch(RunSet $run_set) {
+    public function launch() {
+        $run_set_id = request()->get('run_set');
+        $run_set = RunSet::find($run_set_id);
         //check that the user does not have valid active certificates
         $user = auth()->user();
         if($user->allowed_a_is <= $user->a_is_running) {

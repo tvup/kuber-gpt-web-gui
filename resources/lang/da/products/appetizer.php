99c75e3b26a52caa701b25a81959a5ec3373f353
Torben Evald Hansen <contact@torbenit.dk>
2023-04-29 13:27:03 +0200

    Update "psst" message in Danish language for the "appetizer" product to make it more clear and accurate

--- resources/lang/da/products/appetizer.php
+++ resources/lang/da/products/appetizer.php
@@ -9,6 +9,6 @@ return [
     'you_can_have_it_all' => 'Så får du da også bare det hele',
     'weve_put_it_all_together_for_you' => 'Vi har sat det hele sammen for dig',
     'and_weve_really_enjoyed_it_in_the_long_long_time_its_taken' => 'Det har virkelig taget lang tid, men det har virkelig også været sjovt',
-    'psst_if_you_are_the_type_who_can_also_spend_day_in_and_day_out_then_maybe_you_should_just_put_the_payment_card_away_and_click_here' => '**psst* hvis du er den type, der også kan være bruge dag ud og ind, så skulle du måske lige lægge betalingskortet væk og klikke her',
+    'psst_if_you_are_the_type_who_can_also_spend_day_in_and_day_out_then_maybe_you_should_just_put_the_payment_card_away_and_click_here' => '**psst* hvis du er den type, der også kan bruge dag ud og ind på at nørde, så skulle du måske lige lægge betalingskortet væk og klikke her i stedet',
     'sign_up_and_checkout' => 'Et-trins tilmelding og betal',
 ];

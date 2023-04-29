46365b42444e9cfb815df418366343566937d05b
Torben Evald Hansen <contact@torbenit.dk>
2023-04-29 13:01:34 +0200

    Add translations for product details in Danish, English, and Italian

---
+++ resources/lang/it/products/products.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'introduction-offer-2-days-for-only' => 'OFFERTA DI PRESENTAZIONE - 2 GIORNI A SOLI',
+    'limited_admission' => 'Ingresso limitato',
+    'you_can_renew_for_as_long_as_the_offer_is_active' => 'Puoi rinnovare finché l\'offerta è attiva.',
+    'rent_per_hour_pay_in_advance' => 'Affitto all\'ora - pagamento anticipato',
+    'minimum_6_hours' => 'Minimo 6 ore',
+    'bring_your_own_tokenskeys' => 'Porta i tuoi gettoni / chiavi',
+    'no_recurring_withdrawals' => 'Nessun prelievo ricorrente',
+    'rent_for_24_hours' => 'Affitto per 24 ore',
+    'meter_billing' => '"Fatturazione contatori"',
+    'billed_as_you_use_after_first_hour_which_will_be_billed_for_1_hour_no_matter_what_but_afterwards_you_can_turn_onoff_as_you_wish' => 'Fatturato in base all\'utilizzo - dopo la prima ora che verrà fatturata per 1 ora indipendentemente da cosa, ma in seguito è possibile accendere/spegnere come si desidera',
+    'you_can_pause_for_up_to_1_week' => 'Puoi "mettere in pausa" fino a 1 settimana.',
+    'each_hour_after_24_hours_will_be_settled_at_eur_05_hour' => 'Ogni ora oltre le 24 ore verrà conteggiata a € 0,5/ora',
+    'silver' => 'Argento',
+    'pay_in_advance' => 'Pagare in anticipo',
+    'runs_for_a_month' => 'Funziona per un mese',
+    'gold' => 'Oro',
+    'then_you_just_get_the_whole_chew_bang' => 'Quindi ottieni solo l\'intero masticare',
+    'bring_nothing_just_start_the_window_and_youre_on' => 'Non portare niente: basta aprire la finestra e il gioco è fatto',
+    'gpt_4' => 'GPT-4 !',
+];

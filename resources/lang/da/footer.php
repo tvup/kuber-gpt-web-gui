9c82daa4b7d12be58aa512e41689a278a0f27e82
Torben Evald Hansen <contact@torbenit.dk>
2023-04-29 07:50:37 +0200

    Add footer translations to language files and update footer.blade.php to use translations

---
+++ resources/lang/da/footer.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    'hosted_interactive_self_reflective' => 'Hostet, interaktiv, selvreflekterende AutoGPT i din browser. En intelligent terminal lige til dig.',
+    'company' => 'Virksomhed',
+    'about' => 'Om',
+    'meet_the_team' => 'Mød holdet',
+    'history' => 'Historie',
+    'careers' => 'Karrierer',
+    'autogpt_saas' => 'AutoGPT SaaS',
+    'helpful_links' => 'Hjælpsomme links',
+    'contact' => 'Kontakt',
+    'faqs' => 'Ofte stillede spørgsmål',
+    'live_chat' => 'Live Chat',
+    'legal' => 'Jura',
+    'privacy_policy' => 'Fortrolighedspolitik',
+    'terms_conditions' => 'Vilkår og Betingelser',
+    'returns_policy' => 'Returpolitik',
+    'accessibility' => 'Tilgængelighed',
+
+];
---
+++ resources/lang/en/footer.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    'hosted_interactive_self_reflective' => 'Hosted, interactive, self-reflective AutoGPT in your browser. An intelligent terminal just for you.',
+    'company' => 'Company',
+    'about' => 'About',
+    'meet_the_team' => 'Meet the Team',
+    'history' => 'History',
+    'careers' => 'Careers',
+    'autogpt_saas' => 'AutoGPT SaaS',
+    'helpful_links' => 'Helpful Links',
+    'contact' => 'Contact',
+    'faqs' => 'FAQs',
+    'live_chat' => 'Live Chat',
+    'legal' => 'Legal',
+    'privacy_policy' => 'Privacy Policy',
+    'terms_conditions' => 'Terms & Conditions',
+    'returns_policy' => 'Returns Policy',
+    'accessibility' => 'Accessibility',
+
+];

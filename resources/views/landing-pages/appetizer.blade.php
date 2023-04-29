46a85990294263b7a9fbf2074ac5bb65d3f4b10b
Torben Evald Hansen <contact@torbenit.dk>
2023-04-29 09:54:59 +0200

    Refactor landing page disclaimer container and box styling to improve readability and accessibility

--- resources/views/landing-pages/appetizer.blade.php
+++ resources/views/landing-pages/appetizer.blade.php
@@ -5,25 +5,25 @@
 @section('content')
 
     <div class="flex bg-white" style="height:600px;">
-        <div class="disclaimer-container">
-            <div class="content-top disclaimer-box">
-
-            <p>
-                    {!! __('appetizer.disclaimer')!!}
-                </p>
+        <div class="flex items-center text-center lg:text-left  md:px-12 lg:w-1/2 grid">
+            <div class="disclaimer-container">
+                <div class="disclaimer-box p-4 m-4 bg-yellow-300 text-2xl rounded-md">
+                    <p>
+                        {!! __('appetizer.disclaimer')!!}
+                    </p>
+                </div>
             </div>
-        </div>
-
-        <div class="flex items-center text-center lg:text-left px-8 md:px-12 lg:w-1/2">
             <div>
-                <h2 class="mt-44 text-3xl font-semibold text-gray-800 md:text-4xl">AutoGPT <span class="text-indigo-600">get ready!</span><br/> Your hosted, interactive, and self-reflective AI.
-                </h2>
-                <p class="mt-2 text-sm text-gray-500 md:text-base">{{__('appetizer.its-happening-right-now-its-a-hype-and-now-if-you-want-to-to-cut-the-corners-a-see-it-for-your-self-youre-invited')}}</p>
-                <div class="flex justify-center lg:justify-start mt-6">
-                    <a class="px-4 py-3 bg-gray-900 text-gray-200 text-xs font-semibold rounded hover:bg-gray-800"
-                       href="{{ route('products.index') }}">{{__('appetizer.get_started')}}</a>
-                    <a class="mx-4 px-4 py-3 bg-gray-300 text-gray-900 text-xs font-semibold rounded hover:bg-gray-400"
-                       href="{{ route('about') }}">{{__('appetizer.learn_more')}}</a>
+                <div>
+                    <h2 class="mt-0 text-3xl font-semibold text-gray-800 md:text-4xl">AutoGPT <span class="text-indigo-600">get ready!</span><br/> Your hosted, interactive, and self-reflective AI.
+                    </h2>
+                    <p class="mt-2 text-sm text-gray-500 md:text-base">{{__('appetizer.its-happening-right-now-its-a-hype-and-now-if-you-want-to-to-cut-the-corners-a-see-it-for-your-self-youre-invited')}}</p>
+                    <div class="flex justify-center lg:justify-start mt-6">
+                        <a class="px-4 py-3 bg-gray-900 text-gray-200 text-xs font-semibold rounded hover:bg-gray-800"
+                           href="{{ route('products.index') }}">{{__('appetizer.get_started')}}</a>
+                        <a class="mx-4 px-4 py-3 bg-gray-300 text-gray-900 text-xs font-semibold rounded hover:bg-gray-400"
+                           href="{{ route('about') }}">{{__('appetizer.learn_more')}}</a>
+                    </div>
                 </div>
             </div>
         </div>

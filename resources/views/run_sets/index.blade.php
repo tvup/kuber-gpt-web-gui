2764b4564edb71b6ea53d476eb99719568c5a6a6
Torben Evald Hansen <contact@torbenit.dk>
2023-04-30 22:03:21 +0200

    Fix hyperlink URL on run set index page to correctly display public IP address

--- resources/views/run_sets/index.blade.php
+++ resources/views/run_sets/index.blade.php
@@ -65,7 +65,7 @@
                                     <td><a href="{{route('credentials.index')}}">{{($runSet->id) ? : ' '}}</a></td>
                                     <td>{{$runSet->created_at}}</td>
                                     <td>
-                                        <a href="http://{{$runSet->public_ip ?: auth()->user()->running_port . ':50001'}}">{{$runSet->public_ip ?: auth()->user()->running_port . ':50001'}}</a>
+                                        <a href="http://{{$runSet->public_ip ? $runSet->public_ip . ':50001' : auth()->user()->running_port . ':50001'}}">{{$runSet->public_ip ? $runSet->public_ip . ':50001' : auth()->user()->running_port . ':50001'}}</a>
                                     </td>
                                     <td>{{$runSet->status}}</td>
                                     <td>

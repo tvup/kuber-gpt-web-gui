502f494f34eb0d198ac6848e61961c92b764e5b0
Torben Evald Hansen <contact@torbenit.dk>
2023-05-01 07:52:28 +0200

    Add support for any type of Request object in RunSetController's ip method

--- app/Http/Controllers/API/RunSetController.php
+++ app/Http/Controllers/API/RunSetController.php
@@ -7,10 +7,11 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StorePublicIPForRunSetRequest;
 use App\Models\RunSet;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class RunSetController extends Controller
 {
-    public function ip(StorePublicIPForRunSetRequest $request, RunSet $run_set) {
+    public function ip(Request $request, RunSet $run_set) {
         $ip = $request->input('ip');
 
         $run_set->public_ip = $ip;

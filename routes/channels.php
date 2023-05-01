32208d31f27ec67b96a6f498bc61ccd19aa20819
Torben Evald Hansen <contact@torbenit.dk>
2023-05-01 07:13:56 +0200

    Remove unnecessary logging statements from user authentication channel in channels.php

--- routes/channels.php
+++ routes/channels.php
@@ -14,7 +14,5 @@
 use App\Models\User;
 
 Broadcast::channel('App.User.{id}', function ($user, $id) {
-    logger()->info('Auth channel: ' . $user->id . ' ' . $id);
-    logger()->info('Authenticated? ' . ((int) $user->id === (int) $id) ? 'true' : 'false');
     return (int) $user->id === (int) $id;
 });

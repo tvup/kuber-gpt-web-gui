b03529124e37b7304f6a7ac253effc3c934b5980
Torben Evald Hansen <contact@torbenit.dk>
2023-07-04 08:03:32 +0200

    Refactor authEndpoint URL in bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -41,7 +41,7 @@ import Pusher from 'pusher-js';
 
 var pusher = new Pusher(import.meta.env.VITE_PUSHER_APP_KEY, {
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-    authEndpoint: `${import.meta.env.VITE_APP_URL}broadcasting/auth`,
+    authEndpoint: `broadcasting/auth`,
     auth: {
         headers: {
             "Authorization": "Bearer bPOZyMpJ1GDsS8cAO3ZmM8zJEwz0RXE1sAHWyqSb",

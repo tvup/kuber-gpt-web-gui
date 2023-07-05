d053d46b29a18e4538bfe70253d0f571b7f27c0b
Torben Evald Hansen <contact@torbenit.dk>
2023-07-05 04:42:21 +0200

    Refactor return statement to return active listeners as a JsonResource

--- app/Http/Controllers/ConductorController.php
+++ app/Http/Controllers/ConductorController.php
@@ -67,7 +67,7 @@ class ConductorController extends Controller
         //I proceed if it has no active valid certificates
         $listeners = Redis::publish(config('database.redis.default.create_channel'), json_encode($array));
 
-        return view('run_sets.index', compact('run_set'))->with('msg-success', 'Run set submitted successfully');
+        return new JsonResource(['active_listeners'=>$listeners]);
     }    /**
      * @throws RedisException
      */

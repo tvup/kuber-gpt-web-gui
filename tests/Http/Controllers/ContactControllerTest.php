ee4b5ce878b10827ac5581f6505cf51161168d93
Torben Evald Hanen <contact@torbenit.dk>
2023-10-06 07:09:57 +0200

    Fix contact form submission with repetition* Fix issue where the contact form submission was being marked as spam due to repetition

--- tests/Http/Controllers/ContactControllerTest.php
+++ tests/Http/Controllers/ContactControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Http\Controllers;
 
+use Illuminate\Support\Facades\Cache;
 use Tests\TestCase;
 
 class ContactControllerTest extends TestCase
@@ -11,6 +12,17 @@ class ContactControllerTest extends TestCase
         $this->post('/contact', ['name' => 'Torben', 'email' => 't@a.dk', 'message' => 'Hi! Need help!'])
             ->assertStatus(302)
             ->assertSessionHas('message', 'Thanks for your message! We will get back to you soon!');
+    }
+
+    public function testMailContactFormWithRepetition()
+    {
+        Cache::shouldReceive('get')
+            ->once()
+            ->with('last_contact_form_message')
+            ->andReturn('Hi! Need help!!');
+        $this->post('/contact', ['name' => 'Torben', 'email' => 't@a.dk', 'message' => 'Hi! Need help!!'])
+            ->assertStatus(302)
+            ->assertSessionHas('message', 'Your message was marked as spam. Find someone else to make the live miserable for');
     }
 
     public function testMailContactFormWithSpam()
